de576fbd1dbb008b2cbd3c7edc263c39d58feefd
Nguyễn Đức Vũ Anh <vuanhnguyenduc@gmail.com>
2022-05-04 12:40:41 +0700

    Updates

---
+++ precache-manifest.f2383ff32f9666ffd522aa80362fc87a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e38d2e7265ac67217ce106c89ad0c468",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a1757b3d4225d754d0f",
+    "url": "/static/css/main.1b7d9a9a.chunk.css"
+  },
+  {
+    "revision": "a98094a717cc290b70ad",
+    "url": "/static/js/2.15bc8e61.chunk.js"
+  },
+  {
+    "revision": "6a1757b3d4225d754d0f",
+    "url": "/static/js/main.579e5b2c.chunk.js"
+  },
+  {
+    "revision": "44813b3189bc2808c234",
+    "url": "/static/js/runtime~main.5cf63ebb.js"
+  }
+]);
